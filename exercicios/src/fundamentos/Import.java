9348de51b7240acee86b605b3ae022cdf54316ba
Emerson <emersonsdossantos@gmail.com>
2024-06-19 12:29:17 -0300

    feat: import
    Seção 3: Fundamentos

---
+++ exercicios/src/fundamentos/Import.java
@@ -0,0 +1,19 @@
+package fundamentos;
+
+import java.util.Date;
+
+public class Import {
+
+	public static void main(String[] args) {
+		
+		java.lang.String a = "Boa tarde";
+		java.lang.System.out.println(a);
+		String s = "Bom dia";
+		System.out.println(s);
+		
+		Date d = new Date();
+		System.out.println(d);
+
+	}
+
+}
