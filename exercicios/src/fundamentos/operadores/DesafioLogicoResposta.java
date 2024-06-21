7ae9f9af98ca94cc894e4399709e7f6e9876e597
Emerson <emersonsdossantos@gmail.com>
2024-06-21 12:29:39 -0300

    feat: desafio operadores lógicos  resposta
    Seção: 3 Fundamentos

---
+++ exercicios/src/fundamentos/operadores/DesafioLogicoResposta.java
@@ -0,0 +1,20 @@
+package fundamentos.operadores;
+
+public class DesafioLogicoResposta {
+
+	public static void main(String[] args) {
+		boolean trabalho1 = true;
+		boolean trabalho2 = false;
+		
+		boolean comprouTV50 = trabalho1 && trabalho2;
+		boolean comprouTV32 = trabalho1 ^ trabalho2;
+		boolean comprouSorvete = trabalho1 || trabalho2;
+		
+		System.out.println("Comprou Tv 50\": " + comprouTV50);
+		System.out.println("Comprou Tv 32\": " + comprouTV32);
+		System.out.println("Comprou Sorvete: " + comprouSorvete);
+		System.out.println("Mais saudavel: " + !comprouSorvete);
+
+	}
+
+}
