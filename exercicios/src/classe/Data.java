922a44b734bc36f7fb8e4695e6ea570bb9a042be
Emerson <emersonsdossantos@gmail.com>
2024-07-04 18:34:02 -0300

    feat: desafio metodo
    Seção 5: Classes e Métodos

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -5,5 +5,8 @@ public class Data {
 	int dia;
 	int mes;
 	int ano;
-
+	
+	String formatarData() {
+		return String.format("%d/%d/%d", dia, mes, ano);
+	}
 }
--- exercicios/src/classe/DataTeste.java
+++ exercicios/src/classe/DataTeste.java
@@ -15,8 +15,8 @@ public class DataTeste {
 		data2.mes = 9;
 		data2.ano = 1984;
 		
-		System.out.printf("%d/%d/%d\n", data1.dia, data1.mes, data1.ano);
-		System.out.printf("%d/%d/%d", data2.dia, data2.mes, data2.ano);
+		System.out.println(data1.formatarData());
+		System.out.println(data2.formatarData());
 
 
 	}
