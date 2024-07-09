eb55fcb385163007fd1872f88a3837f4b8b46035
Emerson <emersonsdossantos@gmail.com>
2024-07-08 22:49:21 -0300

    refactor: desafio membros de classe vs instancia
    Seção 5: Classes e Métodos

--- exercicios/src/classe/Produto.java
+++ exercicios/src/classe/Produto.java
@@ -4,16 +4,15 @@ public class Produto {
 	
 	String nome;
 	double preco;
-	double desconto;
+	static double desconto = 0.25;
 	
 	Produto(){ // construtor padrao
 		
 	}
 	
-	Produto(String nomeInicial, double precoInicial, double descontoInicial){
+	Produto(String nomeInicial, double precoInicial){
 		nome = nomeInicial;
 		preco = precoInicial;
-		desconto = descontoInicial;
 	}
 	
 	double precoComDesconto() {
