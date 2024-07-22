c86854fa9792757a62f873a77059a05418ac0766
Emerson <emersonsdossantos@gmail.com>
2024-07-22 19:30:22 -0300

    feat: desafio composicao
    Seção 7: orientação a objeto

---
+++ exercicios/src/oo/composicao/desafio/Produto.java
@@ -0,0 +1,12 @@
+package oo.composicao.desafio;
+
+public class Produto {
+	
+	final double preco;
+	final String nome;
+	
+	Produto(String nome, double preco){
+		this.nome = nome;
+		this.preco = preco;
+	}
+}
