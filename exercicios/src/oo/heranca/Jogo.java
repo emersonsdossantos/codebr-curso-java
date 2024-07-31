43fc2fd0b488b307af9d32bac28ba24f5fc8b71b
Emerson <emersonsdossantos@gmail.com>
2024-07-30 23:16:07 -0300

    feat: herança #01
    Seção 7: orientação a objeto

--- exercicios/src/oo/heranca/Jogo.java
+++ exercicios/src/oo/heranca/Jogo.java
@@ -4,17 +4,22 @@ public class Jogo {
 
 	public static void main(String[] args) {
 		
-		Jogador j1 = new Jogador();
+		Heroi j1 = new Heroi();
 		j1.x = 10;
-		j1.y = 10; 
+		j1.y = 10;
 		
-		j1.andar(Direcao.NORTE);
-		j1.andar(Direcao.LESTE);
-		j1.andar(Direcao.NORTE);
-		j1.andar(Direcao.LESTE);
+		Monstro j2 = new Monstro();
+		j2.x = 10;
+		j2.y = 11; 
+				
+		System.out.println(j1.vida);
+		System.out.println(j2.vida);
 		
-		System.out.println(j1.x);
-		System.out.println(j1.y);
+		j1.atacar(j2);
+		j2.atacar(j1);
+		
+		System.out.println(j1.vida);
+		System.out.println(j2.vida);
 	}
 
 }
