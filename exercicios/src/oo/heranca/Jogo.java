f9f6d81ef39184f1347800b8dbf61f49a0afb775
Emerson <emersonsdossantos@gmail.com>
2024-08-01 23:48:47 -0300

    feat: herança #03
    Seção 7: orientação a objeto

--- exercicios/src/oo/heranca/Jogo.java
+++ exercicios/src/oo/heranca/Jogo.java
@@ -4,9 +4,7 @@ public class Jogo {
 
 	public static void main(String[] args) {
 		
-		Heroi heroi = new Heroi();
-		heroi.x = 10;
-		heroi.y = 10;
+		Heroi heroi = new Heroi(10, 11);
 		
 		Monstro monstro = new Monstro();
 		monstro.x = 10;
