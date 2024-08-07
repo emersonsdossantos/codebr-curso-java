2dd35700867cc87fa04812fd1ea89f77ab47ac14
Emerson <emersonsdossantos@gmail.com>
2024-08-07 10:45:33 -0300

    refactor:  getters/setters
     getters/setters

--- exercicios/src/oo/encapsulamento/Pessoa.java
+++ exercicios/src/oo/encapsulamento/Pessoa.java
@@ -5,14 +5,14 @@ public class Pessoa {
 	private int idade;
 	
 	Pessoa(int idade){
-		alterarIdade(idade);
+		setIdade(idade);
 	}
 	
-	int lerIdade() {
+	int getIdade() {
 		return idade;
 	}
 	
-	void alterarIdade(int novaIdade) {
+	void setIdade(int novaIdade) {
 		novaIdade = Math.abs(novaIdade);
 		if (novaIdade > 0 && novaIdade <=120) {
 			this.idade = novaIdade;			
--- exercicios/src/oo/encapsulamento/PessoaTeste.java
+++ exercicios/src/oo/encapsulamento/PessoaTeste.java
@@ -5,9 +5,9 @@ public class PessoaTeste {
 	public static void main(String[] args) {
 		
 		Pessoa p1 = new Pessoa(-45);
-		p1.alterarIdade(230); 
+		p1.setIdade(230); 
 		
-		System.out.println(p1.lerIdade());
+		System.out.println(p1.getIdade());
 	}
 
 }
