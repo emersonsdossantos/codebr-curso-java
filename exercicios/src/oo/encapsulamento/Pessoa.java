6d9d6703f641f2a3cce6031fb4b334374b1ce4a1
Emerson <emersonsdossantos@gmail.com>
2024-08-07 13:08:48 -0300

    feat: getters/setters
    Seção 7: orientação a objeto

--- exercicios/src/oo/encapsulamento/Pessoa.java
+++ exercicios/src/oo/encapsulamento/Pessoa.java
@@ -2,12 +2,15 @@ package oo.encapsulamento;
 
 public class Pessoa {
 	
+	private String nome;
 	private int idade;
+	private String sobrenome;
 	
-	Pessoa(int idade){
+	Pessoa(String nome, String sobrenome, int idade){
 		setIdade(idade);
+		setNome(nome);
+		setSobrenome(sobrenome);
 	}
-	
 	int getIdade() {
 		return idade;
 	}
@@ -17,6 +20,29 @@ public class Pessoa {
 		if (novaIdade > 0 && novaIdade <=120) {
 			this.idade = novaIdade;			
 		}
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	public String getNomeCompleto() {
+		return getNome() + " " + getSobrenome();
+	}
+	
+	public String toString() {
+		return "Meu nome é " + nome + " e tenho " + idade + " anos";
 	}
 
 }
--- exercicios/src/oo/encapsulamento/PessoaTeste.java
+++ exercicios/src/oo/encapsulamento/PessoaTeste.java
@@ -4,10 +4,13 @@ public class PessoaTeste {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(-45);
+		Pessoa p1 = new Pessoa("Pedro", "Silva", -45);
 		p1.setIdade(230); 
 		
 		System.out.println(p1.getIdade());
+		System.out.println(p1); // toStrig
+		System.out.println(p1.getNomeCompleto());
+		
 	}
 
 }
