197535e23db9c3fbcbf5ff0abd349a984b7ebc25
Emerson <emersonsdossantos@gmail.com>
2025-05-24 18:47:03 -0300

    Classe Tabuleiro #01
    Seção 11: Projeto - Campo Minado (+JUnit 5)

--- campo-minado/src/br/com/cod3r/cm/modelo/Tabuleiro.java
+++ campo-minado/src/br/com/cod3r/cm/modelo/Tabuleiro.java
@@ -1,5 +1,48 @@
 package br.com.cod3r.cm.modelo;
 
-public class Tabuleiro {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Tabuleiro {
+	
+	private int linhas;
+	private int colunas;
+	private int minas;
+	
+	private final List<Campo> campos = new ArrayList<>();
+
+	public Tabuleiro(int linhas, int colunas, int minas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		this.minas = minas;
+		
+		gerarCampos();
+		associarOsVizinhos();
+		sortearMinas();
+		
+	}
+
+
+
+
+	private void gerarCampos() {
+		for (int linha = 0; linha < linhas; linha++) {
+			for (int campo = 0; campo < colunas; campo++) {
+				campos.add(new Campo(linha, campo));
+			}
+		}
+	}
+	
+	private void associarOsVizinhos() {
+		for (Campo c1 : campos) {
+			for (Campo c2 : campos) { 
+				c1.adicionarVizinho(c2);
+			}
+	    }
+	}
+	
+	private void sortearMinas() {
+		
+	}
+	
 }
