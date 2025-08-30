8bc1267baf95ec917b1694b0438a4fecab0b3a26
Emerson <emersonsdossantos@gmail.com>
2025-08-29 23:07:54 -0300

    Campo Minado com Observer #01
    Seção 13: Java Desktop com Swing

---
+++ campo-minado-swing/src/br/com/cod3r/cm/modelo/Tabuleiro.java
@@ -0,0 +1,82 @@
+package br.com.cod3r.cm.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Tabuleiro {
+	
+	private int linhas;
+	private int colunas;
+	private int minas;
+	
+	private final List<Campo> campos = new ArrayList<>();
+
+	public Tabuleiro(int linhas, int colunas, int minas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		this.minas = minas;
+		
+		gerarCampos();
+		associarOsVizinhos();
+		sortearMinas();
+		
+	}
+
+	public void abrir(int linha, int coluna) {
+		try {
+			campos.parallelStream()
+				.filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+				.findFirst()
+				.ifPresent(c -> c.abrir());
+		} catch (Exception e) {
+			// FIXME Ajustar a implementacao do metodo abrir
+			campos.forEach(c -> c.setAberto(true));
+			throw e;
+		}
+	}
+	
+	public void alterarMarcacao(int linha, int coluna) {
+		campos.parallelStream()
+			.filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+			.findFirst()
+			.ifPresent(c -> c.alternarMarcacao());
+	}
+
+	private void gerarCampos() {
+		for (int linha = 0; linha < linhas; linha++) {
+			for (int campo = 0; campo < colunas; campo++) {
+				campos.add(new Campo(linha, campo));
+			}
+		}
+	}
+	
+	private void associarOsVizinhos() {
+		for (Campo c1 : campos) {
+			for (Campo c2 : campos) { 
+				c1.adicionarVizinho(c2);
+			}
+	    }
+	}
+	
+	private void sortearMinas() {
+		long minasArmadas = 0;
+		Predicate<Campo> minado = c -> c.isMinado();
+		
+		do {
+			int aleatorio = (int) (Math.random() * campos.size());
+			campos.get(aleatorio).minar();
+			minasArmadas = campos.stream().filter(minado).count();
+		} while (minasArmadas < minas);
+		
+	}
+	
+	public boolean objetivoAlcancado() {
+		return campos.stream().allMatch(c -> c.objetivoAlcancado());
+	}
+	
+	public void reiniciar() {
+		campos.stream().forEach(c -> c.reiniciar());
+		sortearMinas();
+	}
+}
