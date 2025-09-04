8b0b8214bb8d3b65fecbcb62ef35e1503427ed9f
Emerson <emersonsdossantos@gmail.com>
2025-09-04 19:46:52 -0300

    Interface Gráfica do Campo Minado #02
    Seção 13: Java Desktop com Swing

--- campo-minado-swing/src/br/com/cod3r/cm/visao/PainelTabuleiro.java
+++ campo-minado-swing/src/br/com/cod3r/cm/visao/PainelTabuleiro.java
@@ -1,5 +1,7 @@
 package br.com.cod3r.cm.visao;
 
+import java.awt.GridLayout;
+
 import javax.swing.JPanel;
 
 import br.com.cod3r.cm.modelo.Tabuleiro;
@@ -8,5 +10,12 @@ import br.com.cod3r.cm.modelo.Tabuleiro;
 public class PainelTabuleiro extends JPanel{
 	
 	public PainelTabuleiro(Tabuleiro tabuleiro) {
+		
+		setLayout(new GridLayout(tabuleiro.getLinhas(), tabuleiro.getColunas()));
+		
+		tabuleiro.paraCadaCampo(c -> add(new BotaoCampo(c)));
+		tabuleiro.registrarObservadores(e -> {
+			// TODO mostrar resultado para o usuario
+		});
 	}
 }
