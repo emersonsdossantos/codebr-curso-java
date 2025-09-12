52579765bd506522e7c29c474d3269ab096ef31e
Emerson <emersonsdossantos@gmail.com>
2025-09-12 11:28:04 -0300

    Componente Botao
    Seção 13: Java Desktop com Swing

--- calculadora/src/br/com/cod3r/calc/visao/Teclado.java
+++ calculadora/src/br/com/cod3r/calc/visao/Teclado.java
@@ -1,10 +1,43 @@
 package br.com.cod3r.calc.visao;
 
+import java.awt.Color;
+import java.awt.GridLayout;
+
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Teclado extends JPanel{
+	
+	private final Color COR_CINZA_ESCURO = new Color(64, 63,69);
+	private final Color COR_CINZA_CLARO = new Color(100, 99, 103);
+	private final Color COR_LARANJA = new Color(242, 163, 60);
+	
 	public Teclado() {
+		setLayout(new GridLayout(5, 4));
 		
+		add(new Botao("AC", COR_CINZA_ESCURO));
+		add(new Botao("+/-", COR_CINZA_ESCURO));
+		add(new Botao("%", COR_CINZA_ESCURO));
+		add(new Botao("/", COR_LARANJA));
+		
+		add(new Botao("7", COR_CINZA_CLARO));
+		add(new Botao("8", COR_CINZA_CLARO));
+		add(new Botao("9", COR_CINZA_CLARO));
+		add(new Botao("X", COR_LARANJA));
+		
+		add(new Botao("4", COR_CINZA_CLARO));
+		add(new Botao("5", COR_CINZA_CLARO));
+		add(new Botao("6", COR_CINZA_CLARO));
+		add(new Botao("-", COR_LARANJA));
+		
+		add(new Botao("1", COR_CINZA_CLARO));
+		add(new Botao("2", COR_CINZA_CLARO));
+		add(new Botao("3", COR_CINZA_CLARO));
+		add(new Botao("+", COR_LARANJA));
+		
+		add(new Botao("0", COR_CINZA_CLARO));
+		add(new Botao("0", COR_CINZA_CLARO));
+		add(new Botao(",", COR_CINZA_CLARO));
+		add(new Botao("=", COR_LARANJA));
 	}
 }
