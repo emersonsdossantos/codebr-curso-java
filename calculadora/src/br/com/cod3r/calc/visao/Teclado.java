aa47fdc0776cc7e78a6724942f4ad0cfb3b4ca7b
Emerson <emersonsdossantos@gmail.com>
2025-09-12 21:23:42 -0300

    Lógica da Calculadora #01
    Seção 13: Java Desktop com Swing

--- calculadora/src/br/com/cod3r/calc/visao/Teclado.java
+++ calculadora/src/br/com/cod3r/calc/visao/Teclado.java
@@ -3,11 +3,14 @@ package br.com.cod3r.calc.visao;
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
-public class Teclado extends JPanel{
+public class Teclado extends JPanel implements ActionListener{
 	
 	private final Color COR_CINZA_ESCURO = new Color(64, 63,69);
 	private final Color COR_CINZA_CLARO = new Color(100, 99, 103);
@@ -29,25 +32,25 @@ public class Teclado extends JPanel{
 		c.gridwidth = 1;
 		adicionarBotao("/", COR_LARANJA, c, 3, 0);
 		
-		adicionarBotao("7", COR_CINZA_ESCURO, c, 0, 1);
-		adicionarBotao("8", COR_CINZA_ESCURO, c, 1, 1);
-		adicionarBotao("9", COR_CINZA_ESCURO, c, 2, 1);
+		adicionarBotao("7", COR_CINZA_CLARO, c, 0, 1);
+		adicionarBotao("8", COR_CINZA_CLARO, c, 1, 1);
+		adicionarBotao("9", COR_CINZA_CLARO, c, 2, 1);
 		adicionarBotao("X", COR_LARANJA, c, 3, 1);
 		
-		adicionarBotao("4", COR_CINZA_ESCURO, c, 0, 2);
-		adicionarBotao("5", COR_CINZA_ESCURO, c, 1, 2);
-		adicionarBotao("6", COR_CINZA_ESCURO, c, 2, 2);
+		adicionarBotao("4", COR_CINZA_CLARO, c, 0, 2);
+		adicionarBotao("5", COR_CINZA_CLARO, c, 1, 2);
+		adicionarBotao("6", COR_CINZA_CLARO, c, 2, 2);
 		adicionarBotao("-", COR_LARANJA, c, 3, 2);
 		
-		adicionarBotao("1", COR_CINZA_ESCURO, c, 0, 3);
-		adicionarBotao("2", COR_CINZA_ESCURO, c, 1, 3);
-		adicionarBotao("3", COR_CINZA_ESCURO, c, 2, 3);
+		adicionarBotao("1", COR_CINZA_CLARO, c, 0, 3);
+		adicionarBotao("2", COR_CINZA_CLARO, c, 1, 3);
+		adicionarBotao("3", COR_CINZA_CLARO, c, 2, 3);
 		adicionarBotao("+", COR_LARANJA, c, 3, 3);
 		
 		c.gridwidth = 2;
-		adicionarBotao("0", COR_CINZA_ESCURO, c, 0, 4);
+		adicionarBotao("0", COR_CINZA_CLARO, c, 0, 4);
 		c.gridwidth = 1;
-		adicionarBotao(",", COR_CINZA_ESCURO, c, 2, 4);
+		adicionarBotao(",", COR_CINZA_CLARO, c, 2, 4);
 		adicionarBotao("=", COR_LARANJA, c, 3, 4);
 		
 	}
@@ -57,7 +60,16 @@ public class Teclado extends JPanel{
 		c.gridx = x;
 		c.gridy = y;
 		Botao botao = new Botao(texto, cor);
+		botao.addActionListener(this);
 		add(botao, c);
 		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() instanceof JButton) {
+			JButton botao = (JButton) e.getSource();
+			System.out.println(botao.getText());
+		}
 	}
 }
