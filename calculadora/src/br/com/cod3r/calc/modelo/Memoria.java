ddd6175a4389decc79d9aa1743c21dffdac5da37
Emerson <emersonsdossantos@gmail.com>
2025-09-13 20:44:53 -0300

    Lógica da Calculadora #04
    Seção 13: Java Desktop com Swing

--- calculadora/src/br/com/cod3r/calc/modelo/Memoria.java
+++ calculadora/src/br/com/cod3r/calc/modelo/Memoria.java
@@ -11,7 +11,10 @@ public class Memoria {
 	
 	private static final Memoria instancia = new Memoria();
 	
+	private TipoComando ultimaOperacao = null;
+	private boolean substituir = false;
 	private String textoAtual = "";
+	private String textoBuffer = "";
 	
 	List<MemoriaObservador> observadores = new ArrayList<>();
 	
@@ -34,12 +37,17 @@ public class Memoria {
 	public void processarComando(String texto) {
 		
 		TipoComando tipoComando = detectarComando(texto);
-		System.out.println(tipoComando);
 		
-		if("AC".equals(texto)) {
+		if (tipoComando == null) {
+			return;
+		} else if (tipoComando == TipoComando.ZERAR) {
 			textoAtual = "";
-		} else {
-			textoAtual += texto;
+			textoBuffer = "";
+			substituir = false;
+			ultimaOperacao = null;
+		} else if (tipoComando == TipoComando.NUMERO || tipoComando == TipoComando.VIRGULA) {
+			textoAtual = substituir ? texto : textoAtual + texto;
+			substituir = false;
 		}
 		
 		observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
@@ -68,7 +76,7 @@ public class Memoria {
 				return TipoComando.SOMA;
 			}else if("=".equals(texto)){
 				return TipoComando.IGUAL;
-			}else if(",".equals(texto)){
+			}else if(",".equals(texto) && !textoAtual.contains(",")){
 				return TipoComando.VIRGULA;
 			}
 		}
