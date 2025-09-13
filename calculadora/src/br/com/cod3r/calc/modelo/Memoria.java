4d7df54e3e1d57cc0a40c1120a31ac0a5f2f9854
Emerson <emersonsdossantos@gmail.com>
2025-09-13 00:22:04 -0300

    Lógica da Calculadora #02
    Seção 13: Java Desktop com Swing

--- calculadora/src/br/com/cod3r/calc/modelo/Memoria.java
+++ calculadora/src/br/com/cod3r/calc/modelo/Memoria.java
@@ -1,10 +1,15 @@
 package br.com.cod3r.calc.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Memoria {
 	
 	private static final Memoria instancia = new Memoria();
 	
 	private String textoAtual = "";
+	
+	List<MemoriaObservador> observadores = new ArrayList<>();
 	
 	private Memoria() {
 		
@@ -13,8 +18,23 @@ public class Memoria {
 	public static Memoria getInstancia() {
 		return instancia;
 	}
+	
+	public void adicionarObservador(MemoriaObservador observador) {
+		observadores.add(observador);
+	}
 
 	public String getTextoAtual() {
 		return textoAtual.isEmpty() ? "0" : textoAtual;
+	}
+	
+	public void processarComando(String valor) {
+		
+		if("AC".equals(valor)) {
+			textoAtual = "";
+		} else {
+			textoAtual += valor;
+		}
+		
+		observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
 	}
 }
