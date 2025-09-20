83eda49b5c85ca40003d7ba83f7ceb67d8237506
Emerson <emersonsdossantos@gmail.com>
2025-09-20 19:15:04 -0300

    Criar Tabela Cidades
    Seção 14: Banco de Dados Relacional

---
+++ banco-relacional/criarTabelaCidades.sql
@@ -0,0 +1,19 @@
+create table if not exists cidades(
+    id int unsigned not null auto_increment,
+    nome varchar(255) not null,
+    estado_id int unsigned not null,
+    area decimal(10.5) not null,
+    primary key (id),
+    foreign key (estado_id) references estados(id)
+);
+
+-- create table teste(
+--     id int unsigned not null auto_increment,
+--     primary key (id)
+-- );
+
+-- create table teste(
+--     id int unsigned not null auto_increment primary key
+-- );
+
+drop table if exists teste;
