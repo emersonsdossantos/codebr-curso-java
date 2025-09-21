7a1d6334a1a806626b0d75f3bf13d5f01931635b
Emerson <emersonsdossantos@gmail.com>
2025-09-21 18:11:15 -0300

    Tipos de JOINs
    Seção 14: Banco de Dados Relacional

--- banco-relacional/consultarComJoin.sql
+++ banco-relacional/consultarComJoin.sql
@@ -17,3 +17,11 @@ select
 inner join cidades c on c.estado_id = e.id; 
 
 select * from estados;
+
+select e.nome as Estado, c.nome as Cidade
+from estados e
+left join cidades c on c.estado_id = e.id;
+
+select e.nome as Estado, c.nome Cidade
+from estados e
+right join cidades c on c.estado_id = e.id;
