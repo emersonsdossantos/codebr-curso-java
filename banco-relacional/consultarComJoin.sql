f63a561bec2b583339039d5438cdfc5e417196f2
Emerson <emersonsdossantos@gmail.com>
2025-09-21 17:33:15 -0300

    treinando join
    Seção 14: Banco de Dados Relacional

--- banco-relacional/consultarComJoin.sql
+++ banco-relacional/consultarComJoin.sql
@@ -12,3 +12,8 @@ select
 from estados e
 inner join cidades c on e.id = c.estado_id;
 
+select
+    e.nome as Estados, c.nome as Cidade, regiao as Região from estados e
+inner join cidades c on c.estado_id = e.id; 
+
+select * from estados;
