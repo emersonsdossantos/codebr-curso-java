44b2debdcd77fae9a84948b5e5dae691d935018c
Emerson <emersonsdossantos@gmail.com>
2025-09-20 23:06:06 -0300

    Consultar Múltiplas Tabelas (JOIN)
    Seção 14: Banco de Dados Relacional

---
+++ banco-relacional/consultarComJoin.sql
@@ -0,0 +1,14 @@
+select 
+    e.nome as Estado,
+    c.nome as Cidade,
+    regiao as Região 
+from estados e, cidades c
+where e.id = c.estado_id;
+
+select
+    c.nome as Cidades,
+    e.nome as Estados,
+    regiao as Região
+from estados e
+inner join cidades c on e.id = c.estado_id;
+
